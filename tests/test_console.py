78ceb550ee7eaa1520bbde75118f414dc47207cd
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2023-11-23 00:20:10 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -3,9 +3,10 @@
 Contains the class TestConsoleDocs
 """
 
-HBNBCommand = console.HBNBCommand
+
 import console
 import inspect
+HBNBCommand = console.HBNBCommand
 import pep8
 import unittest
 
@@ -26,16 +27,16 @@ class TestConsoleDocs(unittest.TestCase):
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
-    def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
-
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(HBNBCommand.__doc__, None,
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
+        
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
