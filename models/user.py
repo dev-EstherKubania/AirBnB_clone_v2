d58f8ac10291674fd29182744dde4cc52f9db64d
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2023-11-22 23:22:24 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -13,7 +13,9 @@ from hashlib import md5
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """
+    Representation of a user
+    """
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
@@ -29,11 +31,15 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+        initializes user
+        """
         super().__init__(*args, **kwargs)
 
     def __setattr__(self, name, value):
-        """sets a password with md5 encryption"""
+        """
+        sets a password with md5 encryption
+        """
         if name == "password":
             value = md5(value.encode()).hexdigest()
         super().__setattr__(name, value)
