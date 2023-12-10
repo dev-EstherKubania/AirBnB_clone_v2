6a5734b03f10ebcec469827f06aca56750dfaa0f
dev-EstherKubania <eskury@gmail.com>
2023-12-10 05:05:58 -0800

    generate .tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This script generates a .tgz archive from the contents of the web_static
+folder. The archive is stored in the 'versions' folder with a filename
+following the format: web_static_<year><month><day><hour><minute><second>.tgz
+"""
+
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    :return: The path of the generated archive if successful, otherwise None.
+    """
+    local("mkdir -p versions")
+
+    now = datetime.utcnow()
+    date_string = now.strftime("%Y%m%d%H%M%S")
+
+    archive_name = "web_static_{}.tgz".format(date_string)
+
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
+
+    if result.succeeded:
+        print("Archive created: versions/{}".format(archive_name))
+        return "versions/{}".format(archive_name)
+    else:
+        print("Failed to create archive.")
+        return None
+
+local("pycodestyle --ignore=E501 script_name.py")
+
+do_pack()
+
